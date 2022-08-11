e8b527737c9bc019b56289a30b18a4b83873b131
CHARLY <charlyplazas0621@gmail.com>
2022-08-11 16:24:50 -0500

    se crea mas js

--- src/controllers/ampliarInformacion.js
+++ src/controllers/ampliarInformacion.js
@@ -4,18 +4,20 @@ let contenedorProductos = document.getElementById("fila");
 let informacionProducto = {};
 contenedorProductos.addEventListener("click", function (evento) {
   if (evento.target.classList.contains("img-fluid")) {
+    informacionProducto.foto =
+     ( evento.target.parentElement.querySelector("img").src);
     informacionProducto.nombre =
-      evento.target.parentElement.querySelector("h3").textContent;
+      (evento.target.parentElement.querySelector("h3").textContent);
     informacionProducto.precio =
-      evento.target.parentElement.querySelector("h4").textContent;
+      (evento.target.parentElement.querySelector("h4").textContent);
     informacionProducto.popularidad =
-      evento.target.parentElement.querySelector("p").textContent;
+      (evento.target.parentElement.querySelector("p").textContent);
     informacionProducto.descripcion =
-      evento.target.parentElement.querySelector("h5").textContent;
+     ( evento.target.parentElement.querySelector("h5").textContent);
 
     //guardando un objeto en memoria
 
-    localStorage.setItem(JSON.stringify("producto", informacionProducto));
+    localStorage.setItem("producto", JSON.stringify(informacionProducto));
     window.location.href = "./ampliarinfo.html";
   }
 });
--- src/controllers/detalleProducto.js
+++ src/controllers/detalleProducto.js
@@ -1,4 +1,19 @@
+console.log("hola estoy detallando la compra");
+
 //lamar a la memora
 
-let producto = localStorage.getItem("producto");
+let producto = JSON.parse(localStorage.getItem("producto"));
 console.log(producto);
+
+//referenciar la imagen del producto
+let nombre = document.getElementById("nombreInfo");
+nombre.textContent = producto.nombre;
+
+let foto = document.getElementById("imagenInf");
+foto.src = producto.foto;
+
+let descripcion = document.getElementById("descripcionInfo");
+descripcion.textContent = producto.descripcion;
+
+let precio = document.getElementById("precioInfo");
+precio.textContent = producto.precio;
